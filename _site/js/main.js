331b41b8dab994ac43ebf830b68eff9db7895024
Taylor Milliman <twm013@bucknell.edu>
2016-06-08 14:27:06 -0400

    AI is working

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,18 +1,28 @@
+/*to do: change color when someone winMoves
+show move on mouse hover
+light animation to clean things up.
+*/
+
+
 $(document).ready(function() {
     $("#play").on("click", function() {
+        var comp = {};
         var playerElement = $('input[name=choice]:checked').attr("id");
-        var compElement = $('input[name=choice]:not(:checked)').attr("id");
-        console.log(compElement);
+        comp.element = $('input[name=choice]:not(:checked)').attr("id");
+
+        var difficulty = $("input[name=diff]:checked").attr("id");
         $("#menu").hide();
         $("#board-view").show();
-        game = startGame(playerElement, compElement);
+        comp["diff"] = difficulty;
+        console.log(comp.diff);
+        game = startGame(playerElement, comp);
     });
 
 });
 
 $("#play").text("test");
 
-function startGame(playerElement, compElement) {
+function startGame(playerElement, comp) {
     $("#reset-container").hide();
     var updateTime = 100;
     var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -20,25 +30,41 @@ function startGame(playerElement, compElement) {
     var game;
 
     function gameLoop() {
-      if (isWin(playerElement,board)) {
-        console.log("Winner");
-        clearInterval(game);
-        $("#dispText").text("You Win!");
-        $("#dispText").show();
-        resetGame();
-        return;
-      }
+        if (isWin(playerElement, board)) {
+            clearInterval(game);
+            $("#dispText").text("You Win!");
+            $("#dispText").css('visibility', 'visible');
+            resetGame();
+            return;
+        }
 
-      if (!playerTurn) {
-          compMove(compElement, board);
-          playerTurn = true;
+        if (isTie(board)){
+          clearInterval(game);
+          $("#dispText").text("You tied, maybe next time");
+          $("#dispText").css('visibility', 'visible');
+          resetGame();
           return;
-      }
+        }
+
+        if (isWin(comp.element, board)){
+          clearInterval(game);
+          $("#dispText").text("Nice try, you lost!");
+          $("#dispText").css('visibility', 'visible');
+          resetGame();
+          return;
+        }
+
+        if (!playerTurn) {
+            compMove(comp, board);
+            playerTurn = true;
+            return;
+        }
         $("#board").click(function(event) {
             var id = $(event.target).attr("id");
-            if (id !== undefined && playerTurn) {
-                place(id, playerElement, board);
+            if (id !== undefined && playerTurn && board[id] == 0) {
+                board[id] = playerElement;
                 playerTurn = false;
+                updateFromModel(board);
                 return;
             }
         });
@@ -88,27 +114,144 @@ function isWin(element, board) {
     return false
 }
 
-function compMove(element, board) {
-  console.log("test");
+function compMove(comp, board) {
+    if (comp.diff == "easy") {
+        randMove(comp.element, board);
+    } else if (comp.diff == "med") {
+        smartMove(comp.element, board);
+    } else {
+        minimax(board, comp.element, comp.element);
+    }
+    updateFromModel(board)
+}
+
+function randMove(element, board) {
     var id = Math.floor(Math.random() * 8);
     while (board[id] != 0) {
         id = Math.floor(Math.random() * 8);
     }
-    place(id, element, board);
+    board[id] = element;
 }
 
-function resetGame(){
-  $("#replayModal").modal('show');
-  $("#Yes").on("click", function(){
-    $("#dispText").hide();
-    $("#menu").show();
-    $("#board-view").hide();
-    for (var i=0; i<9; i++){
-      $("#" +i).text("");
+function smartMove(element, board) {
+    var moves = getMoves(board);
+    var opponent = getOpponent(element);
+    var moveScores = [];
+
+    for (var i = 0; i < moves.length; i++) {
+        var copyBoard = board.slice();
+        var move = moves[i];
+        moveScores.push(smartScore(copyBoard, move, element, opponent));
     }
-  });
-  $("#reset-container").show();
-  $("#reset").on("click", function(){
+
+    var max_score_index = moveScores.indexOf(Math.max.apply(null, moveScores));
+    board[moves[max_score_index]] = element;
+}
+
+function smartScore(board, move, element, opponent) {
+    board[move] = element;
+    if (isWin(element,board)) {
+        return 4;
+    }
+    board[move] = opponent;
+    if (isWin(opponent, board)) {
+        return 3;
+    }
+    if (move == 4) {
+        return 2;
+    } else if (move == 0 || move == 2 || move == 6 || move == 8) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function minimax(board, player, turn) {
+    var scores = [];
+    var moves = [];
+    var boardCopy = [];
+    if (isOver(board)) {
+      return score(board, player);
+    }
+
+    var currMoves = getMoves(board);
+    for (var i = 0; i < currMoves.length; i++) {
+        boardCopy = board.slice();
+        boardCopy[currMoves[i]] = turn;
+        scores.push(minimax(boardCopy, player, getOpponent(turn)));
+        moves.push(currMoves[i]);
+    }
+    if (player == turn) {
+        var max_score_index = scores.indexOf(Math.max.apply(null, scores));
+        var move = moves[max_score_index];
+        board[move] = player;
+        console.log(scores);
+        return scores[max_score_index];
+    } else {
+        var min_score_index = scores.indexOf(Math.min.apply(null, scores));
+        var move = moves[min_score_index];
+        board[move] = player;
+        return scores[min_score_index];
+    }
+
+}
+
+function getMoves(board) {
+    var moves = [];
+    for (var i = 0; i < board.length; i++) {
+        if (board[i] == 0) {
+            moves.push(i);
+        }
+    }
+    return moves;
+}
+
+function isOver(board) {
+    if (isWin('X', board) || isWin('O', board) || isTie(board)){
+      return true;
+    }
+    return false;
+}
+
+function getOpponent(player) {
+    if (player == "X") {
+        return 'O';
+    }
+    return 'X'
+}
+
+function score(board, player) {
+    if (isWin(player, board)) {
+        return 10;
+    } else if (isWin(getOpponent(player), board)) {
+        return -10;
+    } else {
+        return 0;
+    }
+}
+
+function updateFromModel(board) {
+    for (var i = 0; i < board.length; i++) {
+        if (board[i] == 'X') {
+            $("#" + i).text('X');
+        } else if (board[i] == 'O') {
+            $("#" + i).text('O');
+        }
+    }
+}
+
+function resetGame() {
     $("#replayModal").modal('show');
-  });
+    $("#Yes").on("click", function() {
+        $("#dispText").css("visibility", 'hidden');
+        $("#menu").show();
+        $("#board-view").hide();
+        for (var i = 0; i < 9; i++) {
+            $("#" + i).text("");
+        }
+    });
+    $("#reset-container").show();
+    $("#reset").on("click", function() {
+        $("#replayModal").modal('show');
+    });
 }
